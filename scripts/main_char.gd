5045a884d935d199eff2818271f12923917c2f1f
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-07 14:13:59 -0300

    fuck dash parado

--- scripts/main_char.gd
+++ scripts/main_char.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
 
 
-const gravity = 1500
-const jump = -1000
+const gravity = 450
+const jump = -350
 const dashx = 5
 const dashy = 4.5
 const FLOOR = Vector2(0,-1)
 var velocity = Vector2.ZERO 
-var speed = 800
-var queda = gravity * 1.3
+var speed = 400
+var queda = gravity * 1.7
 onready var animacao = $AnimatedSprite
 onready var ciclos = $CanvasLayer/ciclos
 onready var timer = $Timer
@@ -32,7 +32,7 @@ func _physics_process(delta):
 		animacao.flip_h = true
 		velocity.x = -speed
 		if Input.is_action_just_pressed("dash"):
-			speed = 800
+			speed = 400
 			speed *= dashx
 			timer.start()
 	elif Input.is_action_pressed("right"):
@@ -40,7 +40,7 @@ func _physics_process(delta):
 		animacao.flip_h = false
 		velocity.x = speed
 		if Input.is_action_just_pressed("dash"):
-			speed = 800
+			speed = 400
 			speed *= dashx
 			timer.start()
 	else:
@@ -50,24 +50,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("up"):
 			animacao.animation = "subir"
 			if Input.is_action_just_pressed("dash"):
-				velocity.y = -1500
+				velocity.y = -350
 				timer.start()
 
 	if Input.is_action_pressed("down"):
 			animacao.animation = "descer"
 			if Input.is_action_just_pressed("dash"):
-					velocity.y = 1500
+					velocity.y = 350
 					timer.start()
-	
-	#dash parado
-	if Input.is_action_just_pressed("dash") and velocity.x == 0:
-		if animacao.is_flipped_h():
-			velocity.x = -1500 * dashx
-			timer.start()
-		else:
-			velocity.x = 1500 * dashx
-			timer.start()
-			
 	
 	#pulo	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -93,4 +83,4 @@ func _physics_process(delta):
 
 
 func _on_Timer_timeout():
-	speed = 800
+	speed = 400
