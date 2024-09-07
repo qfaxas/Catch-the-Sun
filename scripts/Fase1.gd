bbe838245c6e84a70533d54813bab03b7ef2361f
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-07 16:36:57 -0300

    ewqe

---
+++ scripts/Fase1.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var label = $pensamento/Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_pensamento_body_entered(body):
+	if body.name == "notfrieren":
+		label.show()
+
+func _on_pensamento_body_exited(body):
+	if body.name == "notfrieren":
+		label.hide()
