317b84598f46fee07daf28b48b26dc3a472427c6
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-06 23:59:53 -0300

    movimento

--- cenas/main_char.gd
+++ cenas/main_char.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO 
-const gravity = 1000
+const gravity = 45
 const speed = 800
 const jump = -1000
 const FLOOR = Vector2(0,-1)
@@ -22,14 +22,13 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("right"):
 		velocity.x = speed
 	else:
-		return 0
-	if Input.is_action_just_pressed("jump"):
+		velocity.x = 0
 		
-		velocity.y = jump	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump
 		
 	#gravity
-	
-	velocity.y -= gravity
+	velocity.y += gravity
 
 	move_and_slide(velocity, FLOOR)
 
