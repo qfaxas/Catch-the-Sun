f0a7d2f21cedc7a78be228267d182fd1d81dcd97
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-07 00:29:29 -0300

    dash

--- cenas/main_char.gd
+++ cenas/main_char.gd
@@ -2,16 +2,24 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO 
 const gravity = 45
-const speed = 800
+var speed = 800
 const jump = -1000
+const dash = 4
 const FLOOR = Vector2(0,-1)
-onready var notfrieren = $"."
+onready var sprite = $"../StaticBody2D/Sprite"
+onready var timer = $"../Timer"
 
 
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+	
+#coisas do dash
+func _on_Timer_timeout():
+	speed = 800
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -19,6 +27,7 @@ func _physics_process(delta):
 	#caminhar no elemento
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
+		sprite.flip_h
 	elif Input.is_action_pressed("right"):
 		velocity.x = speed
 	else:
@@ -30,5 +39,13 @@ func _physics_process(delta):
 	#gravity
 	velocity.y += gravity
 
+	#dash
+	if Input.is_action_just_pressed("dash"):
+		speed *= dash	
+		timer.start()
+
 	move_and_slide(velocity, FLOOR)
+
+
+
 
