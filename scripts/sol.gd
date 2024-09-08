24d1cf565f472a591986acd137d4597df5f32f5c
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-08 19:12:24 -0300

    es

--- scripts/sol.gd
+++ scripts/sol.gd
@@ -1,2 +1,20 @@
 extends Node2D
 
+onready var recarga = $recarga
+onready var notfrieren = $"../notfrieren"
+
+
+func _on_Area2D_body_entered(body):
+	if body == notfrieren and Autoload.velocidade == 0:
+		recarga.start()
+	
+
+
+func _on_recarga_timeout():
+	if Autoload.poder < 3:
+		Autoload.poder += 1
+		
+func _process(delta):
+	if Autoload.velocidade != 0:
+		recarga.stop()
+	
