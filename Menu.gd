18d3bb455c8805651e1812f8005c19948bffc63a
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-09-08 21:24:24 -0300

    dsadsa

--- Menu.gd
+++ Menu.gd
@@ -2,16 +2,42 @@ extends Control
 
 onready var credits = $credits
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var selectron = 0
+onready var jog = $CanvasLayer/TextureRect2
+onready var credit = $CanvasLayer/TextureRect3
+onready var sai = $CanvasLayer/TextureRect4
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+func _process(delta):
+	if selectron == 0:
+		if Input.is_action_just_pressed("up"):
+			jog.hide()
+			sai.show()
+			selectron = 2
+		elif Input.is_action_just_pressed("down"):
+			jog.hide()
+			credit.show()
+			selectron = 1
+	elif selectron == 1:
+		if Input.is_action_just_pressed("up"):
+			credit.hide()
+			jog.show()
+			selectron = 0
+		elif Input.is_action_just_pressed("down"):
+			credit.hide()
+			sai.show()
+			selectron = 2
+	elif selectron == 2:
+		if Input.is_action_just_pressed("up"):
+			sai.hide()
+			credit.show()
+			selectron = 1
+		elif Input.is_action_just_pressed("down"):
+			sai.hide()
+			jog.show()
+			selectron = 0
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
